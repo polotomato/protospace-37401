d69c2dd5d8ab95bd285965b2823bb0e77a909a65
polotomato <aotomato@gmail.com>
2022-01-13 14:40:06 +0900

    PrototypeモデルとUserモデルの関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :name, :profile, :occupation, :position, presence: true
+
+  has_many :prototypes
 end
